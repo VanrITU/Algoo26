3afffcd70173e6d69e7987f9d6a46f8b3ae0ae81
VanrITU <vanritudk@gmail.com>
2026-02-26 17:32:05 +0100

    bitbybit

--- Bitbybit/src/Main.java
+++ Bitbybit/src/Main.java
@@ -23,40 +23,48 @@ public class Main {
                 String action = st.nextToken();
 
                 if (action.equals("CLEAR")) {
-                    int i = Integer.parseInt(st.nextToken());
-                    register[31 - i] = String.valueOf(0);
+                    int i = 31-(Integer.parseInt(st.nextToken()));
+                    register[i] = String.valueOf(0);
 
 
                 } else if (action.equals("SET")) {
-                    int i = Integer.parseInt(st.nextToken());
-                    register[31 - i] = String.valueOf(1);
+                    int i = 31-(Integer.parseInt(st.nextToken()));
+                    register[i] = String.valueOf(1);
 
                 } else if (action.equals("AND")) {
-                    int i = Integer.parseInt(st.nextToken());
-                    int j = Integer.parseInt(st.nextToken());
+                    int i = 31-(Integer.parseInt(st.nextToken()));
+                    int j = 31-(Integer.parseInt(st.nextToken()));
 
-                    if (register[i].equals("?") || register[j].equals("?")) {
-                        register[31 - i] = "?";
-                    } else if ((Integer.parseInt(register[i])) == 1 & (Integer.parseInt(register[j])) == 1) {
-                        register[31 -i] = "1";
-                    } else if ((Integer.parseInt(register[i])) == 0 & (Integer.parseInt(register[j])) == 0) {
-                        register[31 -i] = "0";
+                    if (register[i].equals("0") || register[j].equals("0")) {
+                        register[i] = "0";
+                    } else if (register[i].equals("1") && register[j].equals("1")) {
+                        register[i] = "1";
                     } else {
-                        register[31 - i] = "0";
+                        register[i] = "?";
                     }
 
-                } else if (action.equals("OR")) {
-                    int i = Integer.parseInt(st.nextToken());
-                    int j = Integer.parseInt(st.nextToken());
 
+                    /*
                     if (register[i].equals("?") || register[j].equals("?")) {
-                        register[31- i] = "?";
+                        register[i] = "?";
                     } else if ((Integer.parseInt(register[i])) == 1 & (Integer.parseInt(register[j])) == 1) {
-                        register[31 - i] = "1";
+                        register[i] = "1";
                     } else if ((Integer.parseInt(register[i])) == 0 & (Integer.parseInt(register[j])) == 0) {
-                        register[31 - i] = "0";
+                        register[i] = "0";
                     } else {
-                        register[31 -i] = "1";
+                        register[i] = "0";
+                    }*/
+
+                } else if (action.equals("OR")) {
+                    int i = 31-(Integer.parseInt(st.nextToken()));
+                    int j = 31-(Integer.parseInt(st.nextToken()));
+
+                    if (register[i].equals("0") && register[j].equals("0")) {
+                        register[i] = "0";
+                    } else if (register[i].equals("1") || register[j].equals("1")) {
+                        register[i] = "1";
+                    } else {
+                        register[i] = "?";
 
                     }
                 }
